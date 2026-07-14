ac970bf95acd545be6feb299a56059b9e4bf17b7
CodingGoose <dbaraian650@gmail.com>
2026-07-14 22:20:39 +0300

    Initial commit

---
+++ body_tracker.py
@@ -0,0 +1,5 @@
+# using openCV and mediapipe
+# to determine joints and head
+
+class BodyTracker:
+    pass
---
+++ simulator.py
@@ -0,0 +1,6 @@
+# the pygame "game" will go here
+# here is where the ball collision simulation happens
+# and where the joints and head will create shapes for the body tracking
+
+class Simulator:
+    pass
